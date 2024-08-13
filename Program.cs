64cc32d9d1854a4cc17c9bc7f746a6c05a428666
leoluisbh <leonardoluisdossantos@gmail.com>
2024-08-13 19:11:57 -0300

    (feat) - Implementando POO no desafio da DIO.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone iphone = new Iphone("123456", "Modelo 1", "111111111", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
